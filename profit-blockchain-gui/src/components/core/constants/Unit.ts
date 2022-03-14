966811500810a0dcbf13a8e02f507b9449bb418c
Profit Crypto <profitcryptonet@gmail.com>
2022-03-14 15:12:48 -0500

    Blockchain Launch with GUI

---
+++ profit-blockchain-gui/src/components/core/constants/Unit.ts
@@ -0,0 +1,7 @@
+enum Unit {
+  PROFIT = 'PROFIT',
+  MOJO = 'MOJO',
+  COLOURED_COIN = 'COLOUREDCOIN',
+}
+
+export default Unit;
---
+++ profit-blockchain-gui/src/constants/PlotterName.ts
@@ -0,0 +1,7 @@
+enum PlotterName {
+  BLADEBIT = 'bladebit',
+  PROFITPOS = 'chiapos',
+  MADMAX = 'madmax',
+}
+
+export default PlotterName;
