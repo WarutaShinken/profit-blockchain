zcomputerwiz/littlelambocoin

tests: Implement tests for the plot sync protocol

tests/plot_sync/test_delta.py
@@ -1,0 +1,97 @@
+import logging
+
+import pytest
+from blspy import G1Element
+
+from chia.plot_sync.delta import Delta, PathListDelta, PlotListDelta
+from chia.protocols.plot_sync_protocol import Plot
+from chia.types.blockchain_format.sized_bytes import bytes32
+from chia.util.ints import uint8, uint64
+
+log = logging.getLogger(__name__)
+
+
+def dummy_plot(path: str) -> Plot:
+    return Plot(path, uint8(32), bytes32(b"\00" * 32), G1Element(), None, G1Element(), uint64(0), uint64(0))
+
+
+@pytest.mark.parametrize(
+    ["delta"],
+    [
+        pytest.param(PathListDelta(), id="path list"),
+        pytest.param(PlotListDelta(), id="plot list"),
+    ],
+)
+def test_list_delta(delta) -> None:
+    assert delta.empty()
+    if type(delta) == PathListDelta:
+        assert delta.additions == []
+    elif type(delta) == PlotListDelta:
+        assert delta.additions == {}
+    else:
+        assert False
+    assert delta.removals == []
+    assert delta.empty()
+    if type(delta) == PathListDelta:
+        delta.additions.append("0")
+    else:
+        delta.additions["0"] = dummy_plot("0")
+    assert not delta.empty()
+    delta.removals.append("0")
+    assert not delta.empty()
+    delta.additions.clear()
+    assert not delta.empty()
+    delta.clear()
+    assert delta.empty()
+
+
+def test_path_list_delta_from_lists() -> None:
+    delta: PathListDelta
+    assert PathListDelta.from_lists([], []).empty()
+    delta = PathListDelta.from_lists(["1"], ["0"])
+    assert delta.additions == ["0"]
+    assert delta.removals == ["1"]
+    delta = PathListDelta.from_lists(["1", "2", "3"], ["1", "2", "3"])
+    assert delta.additions == []
+    assert delta.removals == []
+    delta = PathListDelta.from_lists(["1", "2"], ["1", "2", "3"])
+    assert delta.additions == ["3"]
+    assert delta.removals == []
+    delta = PathListDelta.from_lists(["1"], ["1", "2", "3"])
+    assert delta.additions == ["2", "3"]
+    assert delta.removals == []
+    delta = PathListDelta.from_lists([], ["1", "2", "3"])
+    assert delta.additions == ["1", "2", "3"]
+    assert delta.removals == []
+    delta = PathListDelta.from_lists(["-1"], ["1", "2", "3"])
+    assert delta.additions == ["1", "2", "3"]
+    assert delta.removals == ["-1"]
+    delta = PathListDelta.from_lists(["-1", "1"], ["2", "3"])
+    assert delta.additions == ["2", "3"]
+    assert delta.removals == ["-1", "1"]
+    delta = PathListDelta.from_lists(["-1", "1", "2"], ["2", "3"])
+    assert delta.additions == ["3"]
+    assert delta.removals == ["-1", "1"]
+
+
+def test_delta() -> None:
+    delta: Delta = Delta()
+    assert delta.empty()
+    for d in [delta.valid, delta.invalid, delta.keys_missing]:
+        if type(d) == PlotListDelta:
+            d.additions["0"] = dummy_plot("0")
+        else:
+            d.additions.append("0")
+        assert not delta.empty()
+        d.clear()
+        assert delta.empty()
+    delta.valid.additions["0"] = dummy_plot("0")
+    delta.invalid.additions.append("0")
+    delta.keys_missing.additions.append("0")
+    assert not delta.empty()
+    delta.valid.clear()
+    assert not delta.empty()
+    delta.invalid.clear()
+    assert not delta.empty()
+    delta.keys_missing.clear()
+    assert delta.empty()

tests/plot_sync/test_sender.py
@@ -1,0 +1,99 @@
+import pytest
+
+from chia.plot_sync.exceptions import AlreadyStartedError, InvalidConnectionTypeError
+from chia.plot_sync.sender import ExpectedResponse, Sender
+from chia.plot_sync.util import Constants
+from chia.protocols.plot_sync_protocol import Identifier, Response
+from chia.server.ws_connection import NodeType, ProtocolMessageTypes
+from chia.util.ints import int16, uint64
+from tests.plot_sync.util import get_dummy_connection, plot_sync_identifier
+from tests.setup_nodes import bt
+
+
+def test_default_values() -> None:
+    sender = Sender(bt.plot_manager)
+    assert sender._plot_manager == bt.plot_manager
+    assert sender._connection is None
+    assert sender._sync_id == uint64(0)
+    assert sender._next_message_id == uint64(0)
+    assert sender._messages == []
+    assert sender._last_sync_id == uint64(0)
+    assert not sender._stop_requested
+    assert sender._task is None
+    assert not sender._lock.locked()
+    assert sender._response is None
+
+
+def test_set_connection_values() -> None:
+    farmer_connection = get_dummy_connection(NodeType.FARMER)
+    sender = Sender(bt.plot_manager)
+    # Test invalid NodeType values
+    for connection_type in NodeType:
+        if connection_type != NodeType.FARMER:
+            pytest.raises(
+                InvalidConnectionTypeError,
+                sender.set_connection,
+                get_dummy_connection(connection_type, farmer_connection.peer_node_id),
+            )
+    # Test setting a valid connection works
+    sender.set_connection(farmer_connection)
+    assert sender._connection == farmer_connection
+
+
+@pytest.mark.asyncio
+async def test_start_stop_send_task() -> None:
+    sender = Sender(bt.plot_manager)
+    # Make sure starting/restarting works
+    for _ in range(2):
+        assert sender._task is None
+        await sender.start()
+        assert sender._task is not None
+        with pytest.raises(AlreadyStartedError):
+            await sender.start()
+        assert not sender._stop_requested
+        sender.stop()
+        assert sender._stop_requested
+        await sender.await_closed()
+        assert not sender._stop_requested
+        assert sender._task is None
+
+
+def test_set_response() -> None:
+    sender = Sender(bt.plot_manager)
+
+    def new_expected_response(sync_id: int, message_id: int, message_type: ProtocolMessageTypes) -> ExpectedResponse:
+        return ExpectedResponse(message_type, plot_sync_identifier(uint64(sync_id), uint64(message_id)))
+
+    def new_response_message(sync_id: int, message_id: int, message_type: ProtocolMessageTypes) -> Response:
+        return Response(plot_sync_identifier(uint64(sync_id), uint64(message_id)), int16(int(message_type.value)), None)
+
+    response_message = new_response_message(0, 1, ProtocolMessageTypes.plot_sync_start)
+    assert sender._response is None
+    # Should trigger unexpected response because `Farmer._response` is `None`
+    assert not sender.set_response(response_message)
+    # Set `Farmer._response` and make sure the response gets assigned properly
+    sender._response = new_expected_response(0, 1, ProtocolMessageTypes.plot_sync_start)
+    assert sender._response.message is None
+    assert sender.set_response(response_message)
+    assert sender._response.message is not None
+    # Should trigger unexpected response because we already received the message for the currently expected response
+    assert not sender.set_response(response_message)
+    # Test expired message
+    expected_response = new_expected_response(1, 0, ProtocolMessageTypes.plot_sync_start)
+    sender._response = expected_response
+    expired_identifier = Identifier(
+        uint64(expected_response.identifier.timestamp - Constants.message_timeout - 1),
+        expected_response.identifier.sync_id,
+        expected_response.identifier.message_id,
+    )
+    expired_message = Response(expired_identifier, int16(int(ProtocolMessageTypes.plot_sync_start.value)), None)
+    assert not sender.set_response(expired_message)
+    # Test invalid sync-id
+    sender._response = new_expected_response(2, 0, ProtocolMessageTypes.plot_sync_start)
+    assert not sender.set_response(new_response_message(3, 0, ProtocolMessageTypes.plot_sync_start))
+    # Test invalid message-id
+    sender._response = new_expected_response(2, 1, ProtocolMessageTypes.plot_sync_start)
+    assert not sender.set_response(new_response_message(2, 2, ProtocolMessageTypes.plot_sync_start))
+    # Test invalid message-type
+    sender._response = new_expected_response(3, 0, ProtocolMessageTypes.plot_sync_start)
+    assert not sender.set_response(new_response_message(3, 0, ProtocolMessageTypes.plot_sync_loaded))

tests/plot_sync/util.py
@@ -1,0 +1,53 @@
+import time
+from dataclasses import dataclass
+from typing import Optional
+
+from Crypto.Random import get_random_bytes
+
+from chia.harvester.harvester_api import Harvester
+from chia.plot_sync.sender import Sender
+from chia.protocols.plot_sync_protocol import Identifier
+from chia.server.start_service import Service
+from chia.server.ws_connection import Message, NodeType
+from chia.types.blockchain_format.sized_bytes import bytes32
+from chia.util.ints import uint64
+from tests.time_out_assert import time_out_assert
+
+
+def get_dummy_connection(node_type: NodeType, peer_id=None):
+    @dataclass
+    class WSChiaConnection:
+        connection_type: NodeType = node_type
+        peer_node_id: bytes32 = bytes32(get_random_bytes(32)) if peer_id is None else peer_id
+        peer_host: str = "localhost"
+        peer_port: int = 0
+        last_sent_message: Optional[Message] = None
+
+        async def send_message(self, message: Message):
+            self.last_sent_message = message
+
+    return WSChiaConnection()
+
+
+def plot_sync_identifier(current_sync_id: uint64, message_id: uint64):
+    return Identifier(uint64(int(time.time())), current_sync_id, message_id)
+
+
+async def start_harvester_service(harvester_service: Service) -> Harvester:
+    # Set the `last_refresh_time` of the plot manager to avoid initial plot loading
+    harvester: Harvester = harvester_service._node
+    harvester.plot_manager.last_refresh_time = time.time()
+    await harvester_service.start()
+    harvester.plot_manager.stop_refreshing()
+
+    assert harvester.plot_sync_sender._sync_id == 0
+    assert harvester.plot_sync_sender._next_message_id == 0
+    assert harvester.plot_sync_sender._last_sync_id == 0
+    assert harvester.plot_sync_sender._messages == []
+
+    def wait_for_farmer_connection(plot_sync_sender: Sender) -> bool:
+        return plot_sync_sender._connection is not None
+
+    await time_out_assert(10, wait_for_farmer_connection, True, harvester.plot_sync_sender)
+
+    return harvester

WarutaShinken/profit-blockchain

tests/plot_sync/test_delta.py
@@ -1,0 +1,97 @@
+import logging
+
+import pytest
+from blspy import G1Element
+
+from chia.plot_sync.delta import Delta, PathListDelta, PlotListDelta
+from chia.protocols.harvester_protocol import Plot
+from chia.types.blockchain_format.sized_bytes import bytes32
+from chia.util.ints import uint8, uint64
+
+log = logging.getLogger(__name__)
+
+
+def dummy_plot(path: str) -> Plot:
+    return Plot(path, uint8(32), bytes32(b"\00" * 32), G1Element(), None, G1Element(), uint64(0), uint64(0))
+
+
+@pytest.mark.parametrize(
+    ["delta"],
+    [
+        pytest.param(PathListDelta(), id="path list"),
+        pytest.param(PlotListDelta(), id="plot list"),
+    ],
+)
+def test_list_delta(delta) -> None:
+    assert delta.empty()
+    if type(delta) == PathListDelta:
+        assert delta.additions == []
+    elif type(delta) == PlotListDelta:
+        assert delta.additions == {}
+    else:
+        assert False
+    assert delta.removals == []
+    assert delta.empty()
+    if type(delta) == PathListDelta:
+        delta.additions.append("0")
+    else:
+        delta.additions["0"] = dummy_plot("0")
+    assert not delta.empty()
+    delta.removals.append("0")
+    assert not delta.empty()
+    delta.additions.clear()
+    assert not delta.empty()
+    delta.clear()
+    assert delta.empty()
+
+
+def test_path_list_delta_from_lists() -> None:
+    delta: PathListDelta
+    assert PathListDelta.from_lists([], []).empty()
+    delta = PathListDelta.from_lists(["1"], ["0"])
+    assert delta.additions == ["0"]
+    assert delta.removals == ["1"]
+    delta = PathListDelta.from_lists(["1", "2", "3"], ["1", "2", "3"])
+    assert delta.additions == []
+    assert delta.removals == []
+    delta = PathListDelta.from_lists(["1", "2"], ["1", "2", "3"])
+    assert delta.additions == ["3"]
+    assert delta.removals == []
+    delta = PathListDelta.from_lists(["1"], ["1", "2", "3"])
+    assert delta.additions == ["2", "3"]
+    assert delta.removals == []
+    delta = PathListDelta.from_lists([], ["1", "2", "3"])
+    assert delta.additions == ["1", "2", "3"]
+    assert delta.removals == []
+    delta = PathListDelta.from_lists(["-1"], ["1", "2", "3"])
+    assert delta.additions == ["1", "2", "3"]
+    assert delta.removals == ["-1"]
+    delta = PathListDelta.from_lists(["-1", "1"], ["2", "3"])
+    assert delta.additions == ["2", "3"]
+    assert delta.removals == ["-1", "1"]
+    delta = PathListDelta.from_lists(["-1", "1", "2"], ["2", "3"])
+    assert delta.additions == ["3"]
+    assert delta.removals == ["-1", "1"]
+
+
+def test_delta() -> None:
+    delta: Delta = Delta()
+    assert delta.empty()
+    for d in [delta.valid, delta.invalid, delta.keys_missing]:
+        if type(d) == PlotListDelta:
+            d.additions["0"] = dummy_plot("0")
+        else:
+            d.additions.append("0")
+        assert not delta.empty()
+        d.clear()
+        assert delta.empty()
+    delta.valid.additions["0"] = dummy_plot("0")
+    delta.invalid.additions.append("0")
+    delta.keys_missing.additions.append("0")
+    assert not delta.empty()
+    delta.valid.clear()
+    assert not delta.empty()
+    delta.invalid.clear()
+    assert not delta.empty()
+    delta.keys_missing.clear()
+    assert delta.empty()

tests/plot_sync/test_sender.py
@@ -1,0 +1,101 @@
+import pytest
+
+from chia.plot_sync.exceptions import AlreadyStartedError, InvalidConnectionTypeError
+from chia.plot_sync.sender import ExpectedResponse, Sender
+from chia.plot_sync.util import Constants
+from chia.protocols.harvester_protocol import PlotSyncIdentifier, PlotSyncResponse
+from chia.server.ws_connection import NodeType, ProtocolMessageTypes
+from chia.util.ints import int16, uint64
+from tests.plot_sync.util import get_dummy_connection, plot_sync_identifier
+from tests.setup_nodes import bt
+
+
+def test_default_values() -> None:
+    sender = Sender(bt.plot_manager)
+    assert sender._plot_manager == bt.plot_manager
+    assert sender._connection is None
+    assert sender._sync_id == uint64(0)
+    assert sender._next_message_id == uint64(0)
+    assert sender._messages == []
+    assert sender._last_sync_id == uint64(0)
+    assert not sender._stop_requested
+    assert sender._task is None
+    assert not sender._lock.locked()
+    assert sender._response is None
+
+
+def test_set_connection_values() -> None:
+    farmer_connection = get_dummy_connection(NodeType.FARMER)
+    sender = Sender(bt.plot_manager)
+    # Test invalid NodeType values
+    for connection_type in NodeType:
+        if connection_type != NodeType.FARMER:
+            pytest.raises(
+                InvalidConnectionTypeError,
+                sender.set_connection,
+                get_dummy_connection(connection_type, farmer_connection.peer_node_id),
+            )
+    # Test setting a valid connection works
+    sender.set_connection(farmer_connection)
+    assert sender._connection == farmer_connection
+
+
+@pytest.mark.asyncio
+async def test_start_stop_send_task() -> None:
+    sender = Sender(bt.plot_manager)
+    # Make sure starting/restarting works
+    for _ in range(2):
+        assert sender._task is None
+        await sender.start()
+        assert sender._task is not None
+        with pytest.raises(AlreadyStartedError):
+            await sender.start()
+        assert not sender._stop_requested
+        sender.stop()
+        assert sender._stop_requested
+        await sender.await_closed()
+        assert not sender._stop_requested
+        assert sender._task is None
+
+
+def test_set_response() -> None:
+    sender = Sender(bt.plot_manager)
+
+    def new_expected_response(sync_id: int, message_id: int, message_type: ProtocolMessageTypes) -> ExpectedResponse:
+        return ExpectedResponse(message_type, plot_sync_identifier(uint64(sync_id), uint64(message_id)))
+
+    def new_response_message(sync_id: int, message_id: int, message_type: ProtocolMessageTypes) -> PlotSyncResponse:
+        return PlotSyncResponse(
+            plot_sync_identifier(uint64(sync_id), uint64(message_id)), int16(int(message_type.value)), None
+        )
+
+    response_message = new_response_message(0, 1, ProtocolMessageTypes.plot_sync_start)
+    assert sender._response is None
+    # Should trigger unexpected response because `Farmer._response` is `None`
+    assert not sender.set_response(response_message)
+    # Set `Farmer._response` and make sure the response gets assigned properly
+    sender._response = new_expected_response(0, 1, ProtocolMessageTypes.plot_sync_start)
+    assert sender._response.message is None
+    assert sender.set_response(response_message)
+    assert sender._response.message is not None
+    # Should trigger unexpected response because we already received the message for the currently expected response
+    assert not sender.set_response(response_message)
+    # Test expired message
+    expected_response = new_expected_response(1, 0, ProtocolMessageTypes.plot_sync_start)
+    sender._response = expected_response
+    expired_identifier = PlotSyncIdentifier(
+        uint64(expected_response.identifier.timestamp - Constants.message_timeout - 1),
+        expected_response.identifier.sync_id,
+        expected_response.identifier.message_id,
+    )
+    expired_message = PlotSyncResponse(expired_identifier, int16(int(ProtocolMessageTypes.plot_sync_start.value)), None)
+    assert not sender.set_response(expired_message)
+    # Test invalid sync-id
+    sender._response = new_expected_response(2, 0, ProtocolMessageTypes.plot_sync_start)
+    assert not sender.set_response(new_response_message(3, 0, ProtocolMessageTypes.plot_sync_start))
+    # Test invalid message-id
+    sender._response = new_expected_response(2, 1, ProtocolMessageTypes.plot_sync_start)
+    assert not sender.set_response(new_response_message(2, 2, ProtocolMessageTypes.plot_sync_start))
+    # Test invalid message-type
+    sender._response = new_expected_response(3, 0, ProtocolMessageTypes.plot_sync_start)
+    assert not sender.set_response(new_response_message(3, 0, ProtocolMessageTypes.plot_sync_loaded))

tests/plot_sync/util.py
@@ -1,0 +1,53 @@
+import time
+from dataclasses import dataclass
+from typing import Optional
+
+from Crypto.Random import get_random_bytes
+
+from chia.harvester.harvester_api import Harvester
+from chia.plot_sync.sender import Sender
+from chia.protocols.harvester_protocol import PlotSyncIdentifier
+from chia.server.start_service import Service
+from chia.server.ws_connection import Message, NodeType
+from chia.types.blockchain_format.sized_bytes import bytes32
+from chia.util.ints import uint64
+from tests.time_out_assert import time_out_assert
+
+
+def get_dummy_connection(node_type: NodeType, peer_id=None):
+    @dataclass
+    class WSChiaConnection:
+        connection_type: NodeType = node_type
+        peer_node_id: bytes32 = bytes32(get_random_bytes(32)) if peer_id is None else peer_id
+        peer_host: str = "localhost"
+        peer_port: int = 0
+        last_sent_message: Optional[Message] = None
+
+        async def send_message(self, message: Message):
+            self.last_sent_message = message
+
+    return WSChiaConnection()
+
+
+def plot_sync_identifier(current_sync_id: uint64, message_id: uint64):
+    return PlotSyncIdentifier(uint64(int(time.time())), current_sync_id, message_id)
+
+
+async def start_harvester_service(harvester_service: Service) -> Harvester:
+    # Set the `last_refresh_time` of the plot manager to avoid initial plot loading
+    harvester: Harvester = harvester_service._node
+    harvester.plot_manager.last_refresh_time = time.time()
+    await harvester_service.start()
+    harvester.plot_manager.stop_refreshing()
+
+    assert harvester.plot_sync_sender._sync_id == 0
+    assert harvester.plot_sync_sender._next_message_id == 0
+    assert harvester.plot_sync_sender._last_sync_id == 0
+    assert harvester.plot_sync_sender._messages == []
+
+    def wait_for_farmer_connection(plot_sync_sender: Sender) -> bool:
+        return plot_sync_sender._connection is not None
+
+    await time_out_assert(10, wait_for_farmer_connection, True, harvester.plot_sync_sender)
+
+    return harvester